ad9d413cdc363b60260c88d6093f93f46da385f7
anthony.prieur <anthony.prieur@gmail.com>
2012-05-25 23:59:19 +0200

    Initial project stucture (still not functional yet)

---
+++ apk/src/com/android/perl/config/GlobalConstants.java
@@ -0,0 +1,16 @@
+package com.android.perl.config;
+
+public interface GlobalConstants {
+
+	public static final String PACKAGE_NAME = "com.android.perl";
+	
+	public static final String PERL_MAIN_SCRIPT_NAME = "hello.pl";
+	public static final String PERL_PROJECT_ZIP_NAME = "my_perl_project.zip";
+	
+	public static final String PERL_NICE_NAME = "Perl 5.10.0";
+	public static final String PERL_ZIP_NAME = "perl_r9.zip";
+	public static final String PERL_EXTRAS_ZIP_NAME = "perl_extras_r7.zip";
+
+	public static final String LOG_TAG = "PerlAPK";
+
+}
